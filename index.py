e0892d692d7f97cc18af61ed4e750ec3b143753c
Mark Estudillo <markestudillo@Marks-MacBook-Pro.local>
2017-11-04 20:15:01 -0400

    result.html

--- index.py
+++ index.py
@@ -2,27 +2,34 @@ from flask import Flask, redirect, render_template, request, url_for
 from data import DataFilter
 
 app = Flask(__name__)
-
+restaurantLinks = []
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/choicesMade', methods=['POST'])
+@app.route('/', methods=['POST'])
 def choicesMade():
     # Getting integer value from the user
-    zipCode = request.form['text']
+    zipCode = request.form['zipcode']
     # Calling the html for the choices
-    cuisine = request.form['cuisine']
-    cuisine = cuisine.upper()
-    priceRange = request.form['priceRange']
+    price = request.form['price']
     filterArea = DataFilter()
     # Getting Parameters for the area
-    parameters = filterArea.getLocation(zipCode, priceRange)
+    parameters = filterArea.getLocation(zipCode, price, None)
     location = filterArea.get_results(parameters)
     # Calling the html holding all of the final choices
-    return render_template('index.html', location = location)
+    return render_template('result.html', restaurauntlist=location)
+
+
+@app.route('/', methods=['POST'])
+def imageSelection():
+    choice = int(request.form['choice'])
+    if choice == 1:
+        restaurantLinks.append("Restaurant URL")
+    
+
 
 
 if __name__ == '__main__':
