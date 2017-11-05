9d4c74efa1936cc278e52f6adb66b8529cfe30a8
Mark Estudillo <markestudillo@Marks-MacBook-Pro.local>
2017-11-04 20:33:34 -0400

    choices

--- index.py
+++ index.py
@@ -9,8 +9,8 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/', methods=['POST'])
-def choicesMade():
+@app.route('/choices', methods=['POST'])
+def choices():
     # Getting integer value from the user
     zipCode = request.form['zipcode']
     # Calling the html for the choices
@@ -19,15 +19,20 @@ def choicesMade():
     # Getting Parameters for the area
     parameters = filterArea.getLocation(zipCode, price, None)
     location = filterArea.get_results(parameters)
+    location = {}
+    location["image_url"] = "https://static.pexels.com/photos/70497/pexels-photo-70497.jpeg"
+    location["image_url"] = "https://media2.s-nbcnews.com/j/newscms/2017_10/1200234/10-healthy-fast-food-meals-008-subway-inline-today-170309_89a32509f1b93e969a831a913cc2a2d1.today-inline-large.jpg"
     # Calling the html holding all of the final choices
-    return render_template('result.html', restaurauntlist=location)
+    return render_template('choices.html', restauraunts=location)
 
 
 @app.route('/', methods=['POST'])
 def imageSelection():
     choice = int(request.form['choice'])
-    if choice == 1:
-        restaurantLinks.append("Restaurant URL")
+    if choice != 0:
+        "hello"
+    else:
+        "remove the restaurant"
     
 
 
