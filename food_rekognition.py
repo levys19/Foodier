88c1ea18f76550e91ddfc978215b6b2ffdbe5305
Jimmy Huang <jhuang42@buffalo.edu>
2017-11-05 03:56:01 -0500

    Generalized the food rekognition file, boolean method of is_food that will take an image url and check whether it is mostly food or not

--- food_rekognition.py
+++ food_rekognition.py
@@ -1,37 +1,31 @@
 import urllib.request
 import boto3
-from PIL import Image
 
-if __name__ == "__main__":
 
-    s3 = boto3.client('s3')
+class FoodRekognition:
+    @classmethod
+    def is_food(cls, image_url):
 
-    bucket = 'ubhackingfoodie'
-    file_name = "temp.jpg"
+        s3 = boto3.client('s3')
 
-    client = boto3.client('rekognition')
-    URL = "https://s3-media4.fl.yelpcdn.com/bphoto/q_XLrURFErX9Kbtd_YSqpQ/o.jpg"
-    with urllib.request.urlopen(URL) as url:
-        with open('temp.jpg', 'wb') as f:
-            f.write(url.read())
+        bucket = 'ubhackingfoodie'
+        file_name = "temp.jpg"
 
-    img = Image.open('temp.jpg')
-    s3.upload_file(file_name, bucket, file_name)
+        client = boto3.client('rekognition')
+        with urllib.request.urlopen(image_url) as url:
+            with open('temp.jpg', 'wb') as f:
+                f.write(url.read())
 
-    # img_byte = base64.b64encode(img)
+        s3.upload_file(file_name, bucket, file_name)
 
-    # detected_labels = client.detect_labels(Image={
-    #     'S3Object': {
-    #         'Bucket': bucket,
-    #         'Name': file_name
-    #     }
-    # }, MinConfidence=75)
-
-    # print('Detected labels for Image')
-    # detected_labels = client.detect_labels(Image={'Bytes': f.read()}, MinConfidence=75)
-    # for label in detected_labels['Labels']:
-    #     if label['Name'] == "Person" and label['Confidence'] > 80:
-    #         print("Not food")
-    #         break
-    #     if label['Name'] == "Food":
-    #         print(label['Name'] + ' : ' + str(label['Confidence']))
+        detected_labels = client.detect_labels(Image={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': file_name
+            }
+        }, MinConfidence=75)
+        for label in detected_labels['Labels']:
+            if label['Name'] == "Person" and label['Confidence'] > 75:
+                return False
+            if label['Name'] == "Food":
+                return True
